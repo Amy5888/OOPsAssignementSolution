37c9df5f415cd1079dbc766a56525bd89065fd4d
Amy5888 <98251458+Amy5888@users.noreply.github.com>
2022-03-27 16:14:09 +0530

    Add files via upload

---
+++ GradedAssessmentOops/AdminDepartment.java
@@ -0,0 +1,24 @@
+package GradedAssessmentOops;
+
+public class AdminDepartment extends SuperDepartment {
+	
+	
+	 
+	 public String departmentName() {
+		  
+		  return "Admin Department";
+	  }
+		
+	  public String getTodaysWork() {
+		  
+		  return "Complete Your documents Submissions";
+	  }
+	  
+	  public String getWorkDeadline() {
+		  
+		  return "Complete by EOD";
+	  }
+		
+}
+	
+	
---
+++ GradedAssessmentOops/SuperDepartment.java
@@ -0,0 +1,35 @@
+package GradedAssessmentOops;
+
+public class SuperDepartment {
+
+		
+		
+		  public String departmentName() {
+			  
+			  return "SuperDepartment";
+		  }
+			
+		  public String getTodaysWork() {
+			  
+			  return "No Work as of now";
+		  }
+		  
+		  public String getWorkDeadline() {
+			  
+			  return "Nil";
+		  }
+			
+			
+		  public String isTodayAHoliday() {
+			  
+			  return "Today is not a Holiday";
+		  }
+			
+			
+			
+			
+			
+			    
+
+		}
+
